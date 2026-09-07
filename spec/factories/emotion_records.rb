da63082e0b116f0a324ce23d000c8a0f4efb9ba4
Rick723 <you@daichirikumiku0508@gmail.com>
2026-09-08 07:35:49 +0900

    Add RSpec and FactoryBot test foundation for issue 14

---
+++ spec/factories/emotion_records.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :emotion_record do
+    association :user
+    association :emotion
+    strength { 50 }
+    afterglow { 50 }
+    position_x { 50.0 }
+    position_y { 50.0 }
+    felt_on { Date.current }
+  end
+end
