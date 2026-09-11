b836e01adc676ce1ae600deb9fc416b965050543
Rick723 <you@daichirikumiku0508@gmail.com>
2026-09-11 10:28:56 +0900

    Render today's emotion records on tree for issue 26

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -2,5 +2,12 @@ class TreesController < ApplicationController
   before_action :require_login, only: :show
 
   def show
+    @today = Date.current
+    @emotion_records = current_user.emotion_records
+      .where(felt_on: @today)
+      .includes(:emotion)
+      .order(:id)
+      .load
+    @emotions = Emotion.order(:display_order)
   end
 end
